f32d1ea1a41a46ada94b0bfefc31e8da48ddaadd
cookel2 <eleanor.cook@ons.gov.uk>
2019-07-11 11:33:23 +0100

    create a src/main/resources folder containing an application yml file

---
+++ src/main/java/uk/gov/ons/ctp/integration/contcencucumber/config/AppConfig.java
@@ -0,0 +1,19 @@
+package uk.gov.ons.ctp.integration.contcencucumber.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+
+/** Application Config bean */
+//@EnableRetry
+//@Configuration
+//@ConfigurationProperties
+//@Data
+public class AppConfig {
+//  private ReportSettings reportSettings;
+//  // private Rabbitmq rabbitmq;
+//  private AddressIndexSettings addressIndexSettings;
+//  private CaseServiceSettings caseServiceSettings;
+//  private Logging logging;
+}
--- src/main/java/uk/gov/ons/ctp/integration/contcencucumber/main/Application.java
+++ src/main/java/uk/gov/ons/ctp/integration/contcencucumber/main/Application.java
@@ -1,10 +1,13 @@
 package uk.gov.ons.ctp.integration.contcencucumber.main;
 
 import org.springframework.boot.SpringApplication;
+import uk.gov.ons.ctp.integration.contcencucumber.config.AppConfig;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+  
+    private AppConfig appConfig;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
