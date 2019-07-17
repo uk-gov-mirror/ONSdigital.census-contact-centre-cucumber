531b324a08cf801acb5fbac13cedafb393e63041
cookel2 <eleanor.cook@ons.gov.uk>
2019-07-17 16:13:25 +0100

    tidy up code so that it still runs as a Spring Boot app

---
+++ src/main/java/uk/gov/ons/ctp/integration/contcencucumber/model/GeneratorRequest.java
@@ -0,0 +1,17 @@
+package uk.gov.ons.ctp.integration.contcencucumber.model;
+
+import java.util.List;
+import java.util.Map;
+import lombok.Data;
+import uk.gov.ons.ctp.common.event.EventPublisher.Channel;
+import uk.gov.ons.ctp.common.event.EventPublisher.EventType;
+import uk.gov.ons.ctp.common.event.EventPublisher.Source;
+
+@Data
+public class GeneratorRequest {
+  private EventType eventType;
+  private Source source;
+  private Channel channel;
+
+  private List<Map<String, String>> contexts;
+}
