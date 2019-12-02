4f0bd674587e9e3838b72b6528e3b63888f8a1e3
andrewjohnysons <andrew.johnys@ext.ons.gov.uk>
2019-12-02 13:40:03 +0000

    formatted code

--- src/test/java/uk/gov/ons/ctp/integration/contcencucumber/main/service/impl/ProductServiceImpl.java
+++ src/test/java/uk/gov/ons/ctp/integration/contcencucumber/main/service/impl/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package uk.gov.ons.ctp.integration.contcencucumber.main.service.impl;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -7,8 +8,6 @@ import uk.gov.ons.ctp.common.error.CTPException;
 import uk.gov.ons.ctp.integration.common.product.ProductReference;
 import uk.gov.ons.ctp.integration.common.product.model.Product;
 import uk.gov.ons.ctp.integration.contcencucumber.main.service.ProductService;
-
-import java.util.List;
 
 @Service
 @Validated()
