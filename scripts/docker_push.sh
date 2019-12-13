b8dd454e77ec15d3fcda56381837884b22bf31e7
Phil Whiles <phil.whiles@gmail.com>
2019-12-13 16:04:49 +0000

    CR-640 scripts would be good

---
+++ scripts/docker_push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+echo $GCLOUD_SERVICE_KEY | base64 -d | docker login -u _json_key --password-stdin https://eu.gcr.io
+
+export VERSIONTAG=$TRAVIS_BUILD_ID"-"$TRAVIS_BRANCH
+
+echo "Building with tags [$VERSIONTAG]"
+
+docker build -t eu.gcr.io/census-int-ci/census-contact-centre-cucumber:$VERSIONTAG .
+docker push eu.gcr.io/census-int-ci/census-contact-centre-cucumber:$VERSIONTAG
+
