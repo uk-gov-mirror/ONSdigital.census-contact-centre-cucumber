f6c871112b6516fe58c2f0c852a4ae6124438c51
cookel2 <eleanor.cook@ons.gov.uk>
2019-12-23 15:04:10 +0000

    complete scenario 'I want to check that contact centre service is running'

--- src/test/java/uk/gov/ons/ctp/integration/contcencucumber/cucSteps/smoke/TestServicesAreRunning.java
+++ src/test/java/uk/gov/ons/ctp/integration/contcencucumber/cucSteps/smoke/TestServicesAreRunning.java
@@ -1,18 +1,12 @@
 package uk.gov.ons.ctp.integration.contcencucumber.cucSteps.smoke;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import cucumber.api.java.Before;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -20,27 +14,26 @@ import org.springframework.web.util.UriComponentsBuilder;
 import com.godaddy.logging.Logger;
 import com.godaddy.logging.LoggerFactory;
 
-import uk.gov.ons.ctp.common.error.CTPException;
-import uk.gov.ons.ctp.integration.common.product.model.Product;
-import uk.gov.ons.ctp.integration.contactcentresvc.representation.*;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import uk.gov.ons.ctp.integration.contactcentresvc.representation.FulfilmentDTO;
 import uk.gov.ons.ctp.integration.contcencucumber.cucSteps.TestEndpointsFFData;
-import uk.gov.ons.ctp.integration.contcencucumber.main.service.ProductService;
 
 public class TestServicesAreRunning extends TestEndpointsFFData {
 
 	private static final Logger log = LoggerFactory.getLogger(TestServicesAreRunning.class);
 	private Exception exception;
+	private HttpStatus ccResponse = null;
 	
 	@Given("I access the Fulfilments endpoint")
 	public void i_access_the_Fulfilments_endpoint() {
-		log.info("Check that the Contact Centre service is running and has a fulfilments endpoint");
+		log.info("Check that the Contact Centre service is running");
 		checkContCenSvc_SmokeTest();
 	}
 
 	@Then("I receive a response with a status of {int}")
 	public void i_receive_a_response_with_a_status_of(Integer int1) {
-		log.warn("hello there 7");
-		log.info("hello there 8");
+		assertEquals("The Contact Centre Service may not be running - it does not give a response code of 200", HttpStatus.OK, ccResponse);
 	}
 
 	private void checkContCenSvc_SmokeTest() {
@@ -48,16 +41,13 @@ public class TestServicesAreRunning extends TestEndpointsFFData {
 		final UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(ccBaseUrl).port(ccBasePort)
 				.pathSegment("/fulfilments");
 		
-		String response = "no response";
-		
 		try {
 			ResponseEntity<List<FulfilmentDTO>> fulfilmentResponse = getRestTemplate().exchange(
 					builder.build().encode().toUri(), HttpMethod.GET, null,
 					new ParameterizedTypeReference<List<FulfilmentDTO>>() {
 					});
-			response = fulfilmentResponse.getStatusCode().toString();
-			log.with(response).info("Smoke Test: The response from http://localhost:8171/fulfilments");
-//			fulfilmentDTOList = fulfilmentResponse.getBody();
+			ccResponse = fulfilmentResponse.getStatusCode();
+			log.with(ccResponse).info("Smoke Test: The response from http://localhost:8171/fulfilments");
 		} catch (HttpClientErrorException httpClientErrorException) {
 			this.exception = httpClientErrorException;
 			
