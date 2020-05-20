14d576e31f23d1d3950a14d33c341295a2113b63
cookel2 <eleanor.cook@ons.gov.uk>
2020-05-20 10:55:08 +0100

    reformat SpringIntegrationTest.java

--- src/test/java/uk/gov/ons/ctp/integration/contcencucumber/main/SpringIntegrationTest.java
+++ src/test/java/uk/gov/ons/ctp/integration/contcencucumber/main/SpringIntegrationTest.java
@@ -13,8 +13,12 @@ import uk.gov.ons.ctp.integration.contcencucumber.main.repository.CaseDataReposi
 import uk.gov.ons.ctp.integration.contcencucumber.main.service.ProductService;
 
 @ContextConfiguration(
-    classes = {SpringIntegrationTest.class, ProductService.class, CaseDataRepository.class,
-        CloudDataStore.class},
+    classes = {
+      SpringIntegrationTest.class,
+      ProductService.class,
+      CaseDataRepository.class,
+      CloudDataStore.class
+    },
     loader = SpringBootContextLoader.class,
     initializers = ConfigFileApplicationContextInitializer.class)
 @WebAppConfiguration
