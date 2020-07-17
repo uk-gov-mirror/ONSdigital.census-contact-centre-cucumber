bcb9d12a46bc4726a012c40a197e5b036001e39a
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2020-07-17 11:31:44 +0100

    CR-1079 Removed address for fulfilment event

--- src/test/java/uk/gov/ons/ctp/integration/contcencucumber/cucSteps/fulfilments/TestFulfilmentsEndpoints.java
+++ src/test/java/uk/gov/ons/ctp/integration/contcencucumber/cucSteps/fulfilments/TestFulfilmentsEndpoints.java
@@ -5,7 +5,22 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-
+import java.net.URI;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.util.UriComponentsBuilder;
 import com.godaddy.logging.Logger;
 import com.godaddy.logging.LoggerFactory;
 import cucumber.api.PendingException;
@@ -23,26 +38,9 @@ import io.swagger.client.model.PostalFulfilmentRequestDTO;
 import io.swagger.client.model.Region;
 import io.swagger.client.model.ResponseDTO;
 import io.swagger.client.model.SMSFulfilmentRequestDTO;
-import java.net.URI;
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.util.UriComponentsBuilder;
 import uk.gov.ons.ctp.common.domain.UniquePropertyReferenceNumber;
 import uk.gov.ons.ctp.common.error.CTPException;
 import uk.gov.ons.ctp.common.event.EventPublisher.EventType;
-import uk.gov.ons.ctp.common.event.model.Address;
 import uk.gov.ons.ctp.common.event.model.FulfilmentPayload;
 import uk.gov.ons.ctp.common.event.model.FulfilmentRequest;
 import uk.gov.ons.ctp.common.event.model.FulfilmentRequestedEvent;
@@ -540,7 +538,7 @@ public class TestFulfilmentsEndpoints extends ResetMockCaseApiAndPostCasesBase {
       "a fulfilment request event is emitted to RM for UPRN = {string} addressType = {string} individual = {string} and region = {string}")
   public void
       a_fulfilment_request_event_is_emitted_to_RM_for_UPRN_addressType_individual_and_region(
-          String expectedUprn, String expectedAddressType, String individual, String expectedRegion)
+          String expectedUprnPMB, String expectedAddressType, String individual, String expectedRegionPMB)
           throws CTPException {
     log.info(
         "Check that a FULFILMENT_REQUESTED event has now been put on the empty queue, named "
@@ -602,31 +600,14 @@ public class TestFulfilmentsEndpoints extends ResetMockCaseApiAndPostCasesBase {
         "The FulfilmentRequested event contains an incorrect value of 'caseId'",
         expectedCaseId,
         fulfilmentRequest.getCaseId());
-    Address address = fulfilmentRequest.getAddress();
     // SPG and CE indiv product requests do not need an indiv id creating (see CaseServiceImpl, line
     // 435
 
-    assertEquals(
-        "The FulfilmentRequested event contains an incorrect value of 'uprn'",
-        expectedUprn,
-        address.getUprn());
-    if (expectedAddressType.equalsIgnoreCase("SMS")) {
-      assertNull("SMS Address type should be NULL", address.getAddressType());
+    if (individual.equals("true") && expectedAddressType.equals("HH")) {
+      assertNotNull(fulfilmentRequest.getIndividualCaseId());
     } else {
-      if (individual.equals("true") && address.getAddressType().equals("HH")) {
-        assertNotNull(fulfilmentRequest.getIndividualCaseId());
-      } else {
-        assertNull(fulfilmentRequest.getIndividualCaseId());
-      }
-      assertEquals(
-          "The FulfilmentRequested event contains an incorrect value of 'addressType'",
-          expectedAddressType,
-          address.getAddressType());
+      assertNull(fulfilmentRequest.getIndividualCaseId());
     }
-    assertEquals(
-        "The FulfilmentRequested event contains an incorrect value of 'region'",
-        expectedRegion,
-        address.getRegion());
   }
 
   private ResponseEntity<List<CaseDTO>> getCaseForUprn(String uprn) {
