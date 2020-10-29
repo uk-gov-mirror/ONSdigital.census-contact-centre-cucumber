d5f3f1498c4dbde64134050be3ea15d1f446e40d
deven <devendra.rawat@ext.ons.gov.uk>
2020-10-29 17:14:11 +0000

    CR-1244 Reverted changes

--- src/test/java/uk/gov/ons/ctp/integration/contcencucumber/RunCucumberTestCases.java
+++ src/test/java/uk/gov/ons/ctp/integration/contcencucumber/RunCucumberTestCases.java
@@ -7,7 +7,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
     plugin = {"pretty", "html:target/cucumber"},
-    features = {"src/test/resources/integrationtests/case/testCaseEndpoints.feature"},
+    features = {
+      "src/test/resources/integrationtests/case/smokeTests.feature",
+      "src/test/resources/integrationtests/case/testCaseEndpoints.feature",
+      "src/test/resources/integrationtests/case/testTelephoneEndpoint.feature"
+    },
     glue = {"uk.gov.ons.ctp.integration.contcencucumber.cucSteps.cases"},
     dryRun = false)
 public class RunCucumberTestCases {}
