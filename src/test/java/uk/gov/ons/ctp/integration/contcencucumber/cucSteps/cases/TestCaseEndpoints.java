129d709fa452bf8d93c6fc1a615829385198c316
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2021-02-10 12:36:51 +0000

    CR-1672 Reinstated test. But needed new search criteria to get it working

--- src/test/java/uk/gov/ons/ctp/integration/contcencucumber/cucSteps/cases/TestCaseEndpoints.java
+++ src/test/java/uk/gov/ons/ctp/integration/contcencucumber/cucSteps/cases/TestCaseEndpoints.java
@@ -1035,13 +1035,13 @@ public class TestCaseEndpoints {
     assertEquals(agentId, launchedEvent.getPayload().getResponse().getAgentId());
   }
 
-  @Given("the CC agent cannot find an address that is not of addressType CE, HH, or SPG")
+  @Given("the CC agent has selected an address that is not of addressType CE, HH, or SPG")
   public void the_CC_agent_has_selected_an_address_that_is_not_of_addressType_CE_HH_or_SPG() {
     UriComponentsBuilder builder =
         UriComponentsBuilder.fromHttpUrl(context.getCcBaseUrl())
             .port(context.getCcBasePort())
             .pathSegment("addresses")
-            .queryParam("input", "Public Telephone 13M From 11 Nine Acres");
+            .queryParam("input", "Carpets, Shirley Avenue Southampton");
 
     ResponseEntity<AddressQueryResponseDTO> addressQueryResponse =
         context
@@ -1058,7 +1058,30 @@ public class TestCaseEndpoints {
 
     List<AddressDTO> addressesFound = addressQueryBody.getAddresses();
 
-    assertEquals(0, addressesFound.size());
+    String addressToFind = "Direct Carpets LTD, 2 Shirley Avenue, Southampton, SO15 5NG";
+    String addressFound = "";
+    int indexFound = 500;
+    log.info(
+        "The indexFound value defaults to 500 as that will cause an exception if it does not get reset in the while loop");
+    for (int i = 0; i < addressesFound.size(); i++) {
+      addressFound = addressesFound.get(i).getFormattedAddress();
+      if (addressFound.equals(addressToFind)) {
+        log.with(addressFound).info("This is the address that was found in AIMS");
+        indexFound = i;
+        break;
+      }
+    }
+    assertEquals(
+        "The address query response does not contain the correct address",
+        addressToFind,
+        addressFound);
+
+    uprnStr = addressesFound.get(indexFound).getUprn();
+    String addressTypeFound = addressesFound.get(indexFound).getAddressType().name();
+    log.with(addressTypeFound).info("The addressType of the address found");
+    assertNotEquals("CE", addressTypeFound);
+    assertNotEquals("HH", addressTypeFound);
+    assertNotEquals("SPG", addressTypeFound);
   }
 
   @Then("the CC SVC must also return a {string} error")
